8fde078f73bde37d28e38fcfd3bdc623f9d33f40
Amanda Cheung <ac119@wellesley.edu>
2024-04-25 15:17:15 -0400

    initial firebase setup:

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBDuFphrIjYTvHKXnziZiVKGFUCeWjtchA",
+    authDomain: "ricebook-3f5ae.firebaseapp.com",
+    projectId: "ricebook-3f5ae",
+    storageBucket: "ricebook-3f5ae.appspot.com",
+    messagingSenderId: "900913589804",
+    appId: "1:900913589804:web:47afc2480bd97e39f3ab89",
+    measurementId: "G-WMM8C0MRFS"
+  };
